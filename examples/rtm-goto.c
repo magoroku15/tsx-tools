9449d3de2d9cab18c8207bc9c7f91f4ad86582e8
hideo <hideo@ubuntu.(none)>
2015-05-31 15:58:46 +0900

    add examples

---
+++ examples/rtm-goto.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include "rtm-goto.h"
+
+char buf[1024*1024];
+
+char *
+xabort_code(int xst)
+{
+	//int status = XABORT_CODE(xst);
+	int status = xst;
+
+	if (status & XABORT_RETRY) 
+		return "EXPLICIT";
+	else if (status & XABORT_RETRY) 
+		return "RETRY";
+	else if (status & XABORT_CONFLICT) 
+		return "CONFLICT";
+	else if (status & XABORT_CAPACITY) 
+		return "CAPACITY";
+	else if (status & XABORT_DEBUG) 
+		return "DEBUG";
+	else if (status & XABORT_NESTED) 
+		return "NESTEST";
+	else 
+		return "Unknown"; 	
+}
+
+int
+rtm_test(int sz)
+{
+	unsigned status = 0;
+	int i,j;
+
+	//memset(buf, 0, 1024*1024);
+
+	XBEGIN(abort_handler);
+	 for (i = 0; i < sz; i++) {
+	 	buf[i] = i;
+	 }
+	XEND(); 
+	printf("%d pass\n",sz);
+	return 0;
+
+	XFAIL_STATUS(abort_handler, status);
+	printf("%d aborted %x, %d\n", sz, status, XABORT_CODE(status));
+	//printf("%d fail %x %s\n",sz, status, xabort_code(status));
+	return -1;
+}
+
+main()
+{
+	int sz, lastsz;
+
+	for (sz = 1024; sz < 1024 * 16; sz += 256) {
+		if (rtm_test(sz) == -1)
+			break;
+		lastsz = sz;	
+	}
+	while(1) {
+		if (rtm_test(lastsz) == -1)
+			break;
+	}
+}
+
---
+++ examples/rtm.c
@@ -0,0 +1,72 @@
+#define TYPE_DOUBLE
+#include <malloc.h>
+#include "rtm.h"        /* For gcc 4.8 use immintrin.h and -mrtm */
+
+
+char *buf;
+
+int
+rtm_test(int sz)
+{
+        int status;
+        int i,j;
+#ifdef TYPE_DOUBLE
+	double *dp = (double *) buf;
+#endif
+
+retry:
+        if ((status = _xbegin()) == _XBEGIN_STARTED) {
+
+#ifndef TYPE_DOUBLE
+		for (i = 0; i < sz; i++) {
+			buf[i] = i;
+		}
+#else
+		for (i = 0; i < sz/8; i++) {
+			dp[i] = i + 1;
+		}
+
+		for (i = 0; i < sz/16; i++) {
+			dp[i] = dp[sz/16] + dp[i];
+		}
+
+		for (i = 0; i < sz/16; i++) {
+			dp[i] = dp[sz/16] * dp[i];
+		}
+#endif
+                _xend();
+		printf("%d pass\n" ,sz);
+		return 0;
+        } else {
+		if (status == 0) {
+			printf("%d pass\n" ,sz);
+			return 0;
+		} else {
+			printf("%d aborted %x, %d\n", sz, status, _XABORT_CODE(status));
+			//goto retry;
+			return -1;
+		}
+	}
+}
+
+
+main()
+{
+        int sz, lastsz;
+
+	buf = (char *) memalign(4096, 1024*1024);
+        memset(buf, 0, 1024*1024);
+
+        for (sz = 1024; sz < 1024 * 128; sz += 1024) {
+                if (rtm_test(sz) == -1)
+                        break;
+                lastsz = sz;
+        }
+
+        while(1) {
+                if (rtm_test(lastsz) == -1)
+                        break;
+        }
+
+
+}
